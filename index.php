85bf7afeefa267930229ae09c77323336e6d3b93
juansonjeniffer <jenifferjuanson@gmail.com>
2024-12-05 12:05:05 +0800

    asd

--- index.php
+++ index.php
@@ -87,56 +87,9 @@
   </section>
 
   <!-- SERVICES -->
-  <section id="services">
-    <div class="container">
-      <div class="text-center">
-        <h2>SERVICES</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis expedita possimus, sunt dicta commodi
-          nisi,
-          velit illum eveniet, quia numquam architecto temporibus!</p>
-      </div>
-      <div class="row">
-        <div class="col-lg-4 d-flex align-items-center justify-content-center">
-          <div class="card text-center p-3" style="width: 22rem;">
-            <img src="iconset.webp" class="card-img-top Kiffy mx-auto" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Beutiful Ending</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
-              <a href="" class="btn btn-brand"> Go Somewhere </a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 d-flex align-items-center justify-content-center">
-          <div class="card text-center p-3" style="width: 22rem;">
-            <img src="iconset.webp" class="card-img-top Kiffy mx-auto" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Beutiful Ending</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
-              <a href="" class="btn btn-brand"> Go Somewhere </a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 d-flex align-items-center justify-content-center">
-          <div class="card text-center p-3" style="width: 22rem;">
-            <img src="iconset.webp" class="card-img-top Kiffy mx-auto" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Beutiful Ending</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
-              <a href="" class="btn btn-brand"> Go Somewhere </a>
-            </div>
-          </div>
-        </div>
-
-
-
-      </div>
-    </div>
-  </section>
+<?php
+include 'includes/serbisyo.php';
+?>
 
   <div id="cta" class="d-flex align-items-center">
     <div class="container">
@@ -151,15 +104,9 @@
     </div>
   </div>
 
-  <section id="reviews">
-    <div class="container">
-      <div class="text-center">
-        <h2>REVIEWS</h2>
-        <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. At repellat ullam rem! Perferendis ea veritatis
-          suscipit sequi unde dolorem facilis, voluptatem non, magnam nam laudantium cumque libero iure nostrum
-          voluptas?</p>
-      </div>
-
+  <?php
+  include 'includes/rebyew.php';
+  ?>
 
 
       <!--ITEM-->
